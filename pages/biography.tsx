c04c551f03fa26f7b99f39d24734b9876a0386f8
hos <hovosanoyan@gmail.com>
2021-09-08 18:51:36 +0400

    Add getServerSideProps to biography page

--- pages/biography.tsx
+++ pages/biography.tsx
@@ -1,7 +1,21 @@
+import { GetStaticProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import React from "react";
 import styled from "styled-components";
 
 import { Meta } from "../components/Meta";
+
+export const getServerSideProps: GetStaticProps = async (ctx) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(ctx.locale || "en", [
+        "images",
+        "meta",
+        "albums",
+      ])),
+    },
+  };
+};
 
 interface BiographyProps {
   _?: void;
