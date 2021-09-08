f9910bfa090ff590bfeebe9ac11f54087efe1904
hos <hovosanoyan@gmail.com>
2021-09-08 20:21:21 +0400

    Remove redirects from config

--- next.config.js
+++ next.config.js
@@ -3,14 +3,5 @@ const { i18n } = require("./next-i18next.config");
 /** @type {import('next').NextConfig} */
 module.exports = {
   i18n,
-  redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/en",
-        permanent: true,
-      },
-    ];
-  },
   reactStrictMode: true,
 };
