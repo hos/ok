bcac14fee7ae1f0545416aa8943cb3b69150b7ed
hos <hovosanoyan@gmail.com>
2021-09-09 18:39:01 +0400

    Update sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -6,44 +6,40 @@ module.exports = {
   siteUrl: process.env.HOST_URL || "https://karenohanyan.art",
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  additionalPaths: async () => {
+  additionalPaths: async (config) => {
     const result = [];
 
-    for (const locale of i18n.locales) {
-      const others = i18n.locales.filter((l) => l !== locale);
-      for (const album of albums) {
-        // all possible values
-        for (const img of album.images) {
-          result.push({
-            loc: `/${locale}/${album.path}/${img.fileName.replace(".jpg", "")}`,
-            changefreq: "yearly",
-            priority: 0.7,
-            lastmod: new Date().toISOString(),
+    for (const album of albums) {
+      for (const img of album.images) {
+        result.push({
+          loc: `/${album.path}/${img.fileName.replace(".jpg", "")}`,
+          changefreq: "yearly",
+          priority: 0.7,
+          lastmod: new Date().toISOString(),
+          alternateRefs: i18n.locales.map((locale) => {
+            return {
+              href: `${config.siteUrl}/${locale}`,
+              hreflang: locale,
+              loc: `/${album}/${img.fileName.replace(".jpg", "")}`,
+            };
+          }),
+        });
+      }
 
-            // acts only on '/additional-page-2'
-            alternateRefs: others.map((o) => {
-              return {
-                loc: `/${o}/${album}/${img.fileName.replace(".jpg", "")}`,
-              };
-            }),
-          });
-        }
-
-        for (const [key] of Object.entries(articles)) {
-          result.push({
-            loc: `/${locale}/articles/${key}`,
-            changefreq: "yearly",
-            priority: 0.7,
-            lastmod: new Date().toISOString(),
-
-            // acts only on '/additional-page-2'
-            alternateRefs: others.map((o) => {
-              return {
-                loc: `/${o}/articles/${key}`,
-              };
-            }),
-          });
-        }
+      for (const [key] of Object.entries(articles)) {
+        result.push({
+          loc: `/articles/${key}`,
+          changefreq: "yearly",
+          priority: 0.7,
+          lastmod: new Date().toISOString(),
+          alternateRefs: i18n.locales.map((locale) => {
+            return {
+              href: `${config.siteUrl}/${locale}`,
+              hreflang: locale,
+              loc: `/articles/${key}`,
+            };
+          }),
+        });
       }
     }
 
