6d6f887f38f07a3bbd6507ba68c211fceaaa3adc
hos <hovosanoyan@gmail.com>
2021-09-09 15:42:20 +0400

    Add easter egg

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { GetStaticProps, NextPage } from "next";
 import Image from "next/image";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useEffect, useState } from "react";
 
 import { ImageBlock } from "../components/Block";
 import { CenterView } from "../components/CenterView";
@@ -18,6 +19,45 @@ export const getServerSideProps: GetStaticProps = async (ctx) => {
 
 const Home: NextPage = () => {
   const { t } = useTranslation();
+  const [img, setImg] = useState<HTMLImageElement | null>(null);
+
+  useEffect(() => {
+    const _img = document.querySelector(
+      'img[alt="The Origin of the World"]'
+    ) as HTMLImageElement;
+    setImg(_img);
+  }, [setImg]);
+
+  useEffect(() => {
+    if (!img) {
+      return;
+    }
+
+    const onDown = (e: KeyboardEvent) => {
+      if (e.key.toLowerCase() === "o") {
+        if (!img) {
+          return null;
+        }
+        img.style.transition = "2s";
+        img.style.filter = "invert(100%)";
+      }
+    };
+
+    const onUp = (e: KeyboardEvent) => {
+      if (e.key.toLowerCase() === "o") {
+        img.style.filter = "none";
+      }
+    };
+
+    document.addEventListener("keydown", onDown);
+    document.addEventListener("keyup", onUp);
+
+    return () => {
+      document.removeEventListener("keydown", onDown);
+      document.removeEventListener("keyup", onUp);
+    };
+  }, [img]);
+
   return (
     <CenterView>
       <Meta title={t("Karen Ohanyan")} description={t("description")} />
