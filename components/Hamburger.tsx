df50104edd37d2a44a9b4d55b61e50f146b65597
hos <hovosanoyan@gmail.com>
2021-09-11 00:55:43 +0400

    Hide Hamburger on scroll

--- components/Hamburger.tsx
+++ components/Hamburger.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 
 interface HamburgerProps {
@@ -7,22 +7,49 @@ interface HamburgerProps {
 }
 
 export const _Hamburger: React.FC<HamburgerProps> = (props) => {
+  const ref = useRef<HTMLDivElement | null>();
+
+  useEffect(() => {
+    const handle = () => {
+      if (window.scrollY > 50) {
+        ref.current?.classList.add("hide");
+      } else {
+        ref.current?.classList.remove("hide");
+      }
+    };
+    document.addEventListener("scroll", handle);
+
+    return () => document.removeEventListener("scroll", handle);
+  }, []);
+
   return (
-    <span className={props.className} onClick={props.onClick}>
-      <hr />
-      <hr />
-      <hr />
-    </span>
+    <div className={props.className} ref={(_ref) => (ref.current = _ref)}>
+      <span onClick={props.onClick}>
+        <hr />
+        <hr />
+        <hr />
+      </span>
+    </div>
   );
 };
 
 export const Hamburger = styled(_Hamburger)`
-  background-color: #fff;
   width: 30px;
-  margin: 30px;
+  top: 20px;
+  left: 30px;
   display: none;
+  position: fixed;
+  transition: 0.5s;
 
-  & > hr {
+  &.hide {
+    opacity: 0;
+  }
+
+  .show-menu & {
+    opacity: 1;
+  }
+
+  & hr {
     margin-top: 2px;
     margin-bottom: 2px;
     width: 100%;
