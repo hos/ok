cddf9bad55fef98932ae61a038e7cc26f8463d70
hos <hovosanoyan@gmail.com>
2021-09-14 00:05:39 +0400

    Add workgin google tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -48,11 +48,16 @@ export default class MyDocument extends Document {
       `,
             }}
           />
-          <NextScript>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          {/* eslint-disable-next-line */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PXMSHCX');`}</NextScript>
+})(window,document,'script','dataLayer','GTM-PXMSHCX');`,
+            }}
+          />
         </Head>
         <body>
           <noscript>
