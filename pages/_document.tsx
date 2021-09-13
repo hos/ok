e4d2c6af6cfeef1fdc5e599f1e8a8151a48edf07
hos <hovosanoyan@gmail.com>
2021-09-13 22:37:57 +0400

    Add tag manager iframe

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -66,6 +66,14 @@ export default class MyDocument extends Document {
           />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-NV7B3T3"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
