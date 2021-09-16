906417b9b7ed60b476e6c8234384e9ed0d83a459
hos <hovosanoyan@gmail.com>
2021-09-16 12:03:16 +0400

    Add headers property in config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,20 @@ const { i18n } = require("./next-i18next.config");
 module.exports = {
   i18n,
   reactStrictMode: true,
+  headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     minimumCacheTTL: 60 * 60 * 24 * 30,
   },
