2d3138ea72d9d6aa2045a252e18362d43ef6b316
hos <hovosanoyan@gmail.com>
2021-09-19 14:32:15 +0400

    Use dynamic import

--- pages/[...album].tsx
+++ pages/[...album].tsx
@@ -16,11 +16,8 @@ import { Meta } from "../components/Meta";
 import albums from "../data/albums.json";
 import { useAlbumNav } from "../hooks/useAlbumNav";
 import { useShortcut } from "../hooks/useShortcut";
-import Images from "../lib/images";
 import { useLD } from "../lib/ld";
 import { i18n } from "../next-i18next.config";
-
-type ImageName = keyof typeof Images;
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths: string[] = [];
@@ -127,7 +124,7 @@ export const ImagePage: React.FC<ImagePageProps> = () => {
               width="800"
               height="500"
               objectFit="contain"
-              src={Images[image.fileName as ImageName]}
+              src={`/images/large/${image.fileName}`}
               alt={t(`images:${image.fileName}`)}
             />
             <Title>{t(`images:${image.fileName}`)}</Title>
