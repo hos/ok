9ead03b6a9e5b4f6724c5d9352440d1e8a063df0
hos <44600044+hos@users.noreply.github.com>
2024-06-16 20:13:45 +0400

    Add sliding navigation for images (#9)
    * Add sliding navigation for images
    
    * If text don't have link just render inside div
    
    * Switch languages of momik's texts
    
    * Add image slider
    
    * Justify text

--- src/app/[locale]/texts/page.tsx
+++ src/app/[locale]/texts/page.tsx
@@ -13,18 +13,29 @@ export default async function TextsPage() {
     <div className="w-full max-w-[800px] mx-auto flex flex-col justify-start p-5">
       <Meta />
 
-      <div className="m-auto pb-28 max-w-2xl">
+      <div className="m-auto pb-28 max-w-2xl flex flex-col gap-6">
         {Object.entries(texts).map(([key, value]) => {
           const title = t(key);
           if (title === key) {
             return null;
           }
+
+          if (value.hasLink) {
+            return (
+              <Link href={`/${locale}/texts/${key}`} key={key} passHref>
+                <h4 className="text-red font-medium">{t(`texts.${key}`)}</h4>
+                <p className="text-sm">
+                  {/* @ts-ignore */}
+                  {value.noAuthor ? null : t(`texts.${value.author}`)}
+                  {"year" in value ? ` / ${value.year}` : ""}
+                </p>
+                <p className="text-xs">{t(key)}</p>
+              </Link>
+            );
+          }
+
           return (
-            <Link
-              href={value.hasLink ? `/${locale}/texts/${key}` : "#"}
-              key={key}
-              passHref
-            >
+            <div className="cursor-default" key={key}>
               <h4 className="text-red font-medium">{t(`texts.${key}`)}</h4>
               <p className="text-sm">
                 {/* @ts-ignore */}
@@ -32,7 +43,7 @@ export default async function TextsPage() {
                 {"year" in value ? ` / ${value.year}` : ""}
               </p>
               <p className="text-xs">{t(key)}</p>
-            </Link>
+            </div>
           );
         })}
       </div>
