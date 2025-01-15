3c3c67f90b65013d1cb3a87554db584cc007fdc9
hos <hovosanoyan@gmail.com>
2025-01-15 17:11:33 +0400

    update packages

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,15 +1,19 @@
 import { getRequestConfig } from "next-intl/server";
 
-export default getRequestConfig(async ({ locale }) => ({
-  timeZone: "Asia/Yerevan",
-  messages: {
-    albums: (await import(`../public/locales/${locale}/albums.json`)).default,
-    media: (await import(`../public/locales/${locale}/media.json`)).default,
-    ...(await import(`../public/locales/${locale}/texts-body.json`)).default,
-    texts: (await import(`../public/locales/${locale}/texts.json`)).default,
-    exhibitions: (await import(`../public/locales/${locale}/exhibitions.json`))
-      .default,
-    images: (await import(`../public/locales/${locale}/images.json`)).default,
-    ...(await import(`../public/locales/${locale}/meta.json`)).default,
-  },
-}));
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await requestLocale;
+  return {
+    timeZone: "Asia/Yerevan",
+    messages: {
+      albums: (await import(`../public/locales/${locale}/albums.json`)).default,
+      media: (await import(`../public/locales/${locale}/media.json`)).default,
+      ...(await import(`../public/locales/${locale}/texts-body.json`)).default,
+      texts: (await import(`../public/locales/${locale}/texts.json`)).default,
+      exhibitions: (
+        await import(`../public/locales/${locale}/exhibitions.json`)
+      ).default,
+      images: (await import(`../public/locales/${locale}/images.json`)).default,
+      ...(await import(`../public/locales/${locale}/meta.json`)).default,
+    },
+  };
+});
